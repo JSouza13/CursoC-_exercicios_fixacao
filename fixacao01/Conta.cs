d5e3f0bd62a6932256249f27e199bf3fc0f9ce51
João Souza <jooao_souza@hotmail.com>
2018-11-11 15:19:11 -0200

    Aula 32 a 34 - Exercicio 01 de fixação

---
+++ fixacao01/Conta.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace fixacao01
+{
+    class Conta
+    {
+        public int numero { get; private set;}
+        public string titular { get; set; }
+        public double saldo { get; private set; }
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+
+        public Conta(int numero, string titular)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            saldo = 0.00;
+        }
+
+        public void Depositar(double quantia)
+        {
+            saldo += quantia;
+        }
+
+        public void Sacar(double quantia)
+        {
+            saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + numero
+                + ", Titular: "
+                + titular
+                + ", Saldo: $ "
+                + saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- fixacao01/Program.cs
+++ fixacao01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace fixacao01
 {
@@ -10,6 +7,42 @@ namespace fixacao01
     {
         static void Main(string[] args)
         {
+            Conta novaConta;
+
+            Console.Write("Digite o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial(s / n) ? ");
+            char depositoInicio = char.Parse(Console.ReadLine());
+            if (depositoInicio == 's' || depositoInicio == 'S')
+            {
+                Console.Write("Digite o valor do depósito inicial: ");
+                double valorInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                novaConta = new Conta(numero, nome, valorInicial);
+            }
+            else
+            {
+                //sobrecarga do meu construtor
+                novaConta = new Conta(numero, nome);
+            }
+
+            Console.WriteLine("\nConta criada:");
+            Console.WriteLine(novaConta);
+
+            Console.Write("\nDigite um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            novaConta.Depositar(deposito);
+            Console.WriteLine("Conta atualizado: ");
+            Console.WriteLine(novaConta);
+
+            Console.Write("\nDigite um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            novaConta.Sacar(saque);
+            Console.WriteLine("Conta atualizado: ");
+            Console.WriteLine(novaConta);
+
+            Console.ReadLine();
         }
     }
 }
