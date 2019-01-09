1984371421519d5df157810a802abb0962d4d78f
Joao Vitor <jooao_souza@hotmail.com>
2019-01-09 15:22:03 -0200

    Aula 135 - Conclusão do exercicio proposto de Métodos abstratos.

--- MetodosAbastratos/Program.cs
+++ MetodosAbastratos/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using MetodosAbastratos.Entities;
+using MetodosAbastratos.Entities.Enums;
+
 
 namespace MetodosAbastratos
 {
@@ -10,6 +11,40 @@ namespace MetodosAbastratos
     {
         static void Main(string[] args)
         {
+            List<Shape> list = new List<Shape>();
+
+            Console.Write("Enter the number of shapes: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i =0; i<n;i++)
+            {
+                Console.WriteLine($"Shape #{i} data: ");
+                Console.Write("Rectangle or Circle (r/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Color (Black/Blue/Red): ");
+                Color color = (Color)Enum.Parse(typeof(Color), Console.ReadLine());
+                if(ch=='r')
+                {
+                    Console.Write("Width: ");
+                    double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Height: ");
+                    double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Rectangle(width, height, color));
+                }
+                else
+                {
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Circle(radius, color));
+                }                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("SHAPE AREAS: ");
+            foreach (Shape shape in list)
+            {
+                Console.WriteLine(shape.Area().ToString("F2", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
