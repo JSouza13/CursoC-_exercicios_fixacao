8ddbaa2666f06c63384564cd89d97e1b6d7f384e
Joao Vitor <jooao_souza@hotmail.com>
2019-01-09 13:17:28 -0200

    Aula 132 - Exercicio proposto

---
+++ ExercicioProposto/Entities/ImportedProduct.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace ExercicioProposto.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee)
+            :base(name,price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string priceTag()
+        {
+            return Name
+                + " $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Customs fee: $ " + CustomsFee + ")";
+        }
+    }
+}
